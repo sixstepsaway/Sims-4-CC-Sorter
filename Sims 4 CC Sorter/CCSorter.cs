c21307860555a2aca3c5aaa2dae1c14ad8679ef0
sixstepsaway <61681613+sixstepsaway@users.noreply.github.com>
2023-04-19 12:02:58 +0100

    Project now finds incorrect package versions in CC folders.

--- Sims 4 CC Sorter/CCSorter.cs
+++ Sims 4 CC Sorter/CCSorter.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using s4pi.Interfaces;
 using s4pi.Package;
+using s4pi.Settings;
 
 
 namespace FindPackages.PackageDiscovery {
@@ -12,6 +14,7 @@ namespace FindPackages.PackageDiscovery {
         public List<FileInfo> allPackages = new List<FileInfo>();
         public List<FileInfo> notPackages = new List<FileInfo>();
         public string[] packageContents;
+        public string filename = "";
 
 
         public void IdentifyPackages(string ModFolder){
@@ -27,17 +30,88 @@ namespace FindPackages.PackageDiscovery {
             }
         }
 
-        void LineType<T>(T LineType) {
+        void GetTypeOf<T>(T LineType) {
             Console.WriteLine(typeof(T));
         }
         public void GetPackageInfo(FileInfo packageFile){
-            //FileStream getInfo = new FileStream(packageFile.FullName, FileMode.OpenOrCreate);
+            /*FileStream getInfo = new FileStream(packageFile.FullName, FileMode.OpenOrCreate);
             Console.WriteLine("Opened " + packageFile.Name);
             dynamic packageCont = File.ReadAllLines(packageFile.FullName);
             foreach (dynamic item in packageCont) {
-                LineType(item);
+                GetTypeOf(item);
                 //Console.Write(item);
             }
+            var fileContents = System.IO.File.OpenRead(packageFile);
+            filename = fileContents;
+            Console.Write(fileContents.GetType);
+            FileStream packageContents = File.OpenRead(packageFile.FullName);
+            GetPackageContents(packageContents);            
+            GetTypeOf(fs);*/
+            
+        }
+
+        public void FindBrokenPackages (FileInfo package, String logfile, int packagecount, int game){
+            FileStream dbpfFile = new FileStream(package.FullName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            BinaryReader readFile = new BinaryReader(dbpfFile);
+            StreamWriter putContentsIntoTxt = new StreamWriter(logfile, append: true);
+            //Console.WriteLine("Checking " + package.FullName);
+            string test;
+            test = Encoding.ASCII.GetString(readFile.ReadBytes(4));
+            //Console.WriteLine(test);
+            var statement = "";
+            var time = "";
+            if (test != "DBPF") {
+                readFile.Close();
+                time = DateTime.Now.ToString("h:mm:ss tt");
+                statement = time + ": " + package.FullName + " is either not a package or is broken.";
+                Console.WriteLine(statement);
+                putContentsIntoTxt.WriteLine(statement);
+                putContentsIntoTxt.Close();
+                return;
+            }
+
+            uint major = readFile.ReadUInt32();
+            test = major.ToString();
+            //Console.WriteLine(test);
+            
+            uint minor = readFile.ReadUInt32();
+            test = minor.ToString();
+            //Console.WriteLine(test);
+
+            
+
+            if (major is 1 && minor is 1) {
+                if (game is 2) {
+                    //
+                } else {
+                    time = DateTime.Now.ToString("h:mm:ss tt");
+                    statement = time + ": " + package.FullName + " is a sims 2 file.";      
+                    Console.WriteLine(statement);
+                    putContentsIntoTxt.WriteLine(statement);
+                    putContentsIntoTxt.Close();
+                }                
+            } else if (major is 2 && minor is 1) {
+                if (game is 4) {
+                    //
+                } else {
+                    time = DateTime.Now.ToString("h:mm:ss tt");
+                    statement = time + ": " + package.Name + " is a sims 4 file.";      
+                    Console.WriteLine(statement);
+                    putContentsIntoTxt.WriteLine(statement);
+                    putContentsIntoTxt.Close();
+                }                
+            } else if (major is 2 && minor is 0) {
+                if (game is 3) {
+                    //
+                } else {
+                    time = DateTime.Now.ToString("h:mm:ss tt");
+                    statement = time + ": " + package.FullName + " is a sims 3 file.";      
+                    Console.WriteLine(statement);
+                    putContentsIntoTxt.WriteLine(statement);
+                    putContentsIntoTxt.Close();
+                }                
+            }
+            putContentsIntoTxt.Close();
         }
     }
 }
