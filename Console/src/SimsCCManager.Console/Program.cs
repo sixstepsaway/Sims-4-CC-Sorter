ecbe64013d42ebf17b12045fe35c7f22beede329
WandaSoule <61717568+WandaSoule@users.noreply.github.com>
2023-05-04 23:26:11 -0500

    i work very hard every day and i have no money
    I changed it to 64 and it e-O put random bullshit numbers before teh start of the instanceID but we arent misaligned anymore for typeID and group ID for the rest of teh entries

---
+++ Console/src/SimsCCManager.Console/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
+using SSAGlobals;
+using SimsCCManager.Packages.Containers;
+using Newtonsoft.Json;
+using SimsCCManager.Packages.Initial;
+using SimsCCManager.Packages.Sims4Search;
+
+namespace SimsCCManager.CMD
+{     
+    class Program { 
+        
+        static void Main(string[] args)
+        {
+            GlobalVariables globals = new GlobalVariables();
+            TypeListings typeListings = new TypeListings();
+            InitialProcessing initial = new InitialProcessing();
+            LoggingGlobals log = new LoggingGlobals();
+            S4PackageSearch sims4s = new S4PackageSearch();
+            ParallelOptions parallelSettings = new ParallelOptions() { MaxDegreeOfParallelism = 200};
+            TypeListings.AllTypesS2 = typeListings.createS2TypeList();
+            TypeListings.AllTypesS3 = typeListings.createS3TypeList();
+            TypeListings.AllTypesS4 = typeListings.createS4TypeList();
+            TypeListings.S2BuyFunctionSort = typeListings.createS2buyfunctionsortlist();
+            TypeListings.S2BuildFunctionSort = typeListings.createS2buildfunctionsortlist();
+            
+
+            
+
+            string folder = "E:\\Programming\\GitHub\\JessProjects\\FileTest";
+            
+            globals.Initialize(0, folder);
+            log.InitializeLog();
+
+            initial.IdentifyPackages();
+
+            Parallel.ForEach(GlobalVariables.justPackageFiles, parallelSettings, file => 
+            {
+                initial.FindBrokenPackages(file.FullName);
+            });
+            
+            Parallel.ForEach(GlobalVariables.workingPackageFiles, parallelSettings, file => 
+            {
+                initial.IdentifyGames(file.Location);
+            });
+
+            Parallel.ForEach(GlobalVariables.gamesPackages, parallelSettings, file => 
+            {
+                initial.IdentifyGames(file.Location);
+            });
+
+            foreach (PackageFile package in GlobalVariables.gamesPackages){
+                sims4s.SearchS4Packages(package.Location);
+            }
+
+            
+            foreach (SimsPackage pack in Containers.allSims4Packages){
+                log.MakeLog(pack.ToString(), false);
+            }
+        }
+    }
+}
